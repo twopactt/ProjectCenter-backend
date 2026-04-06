8ab9043d7b07bc0f5b6c8e350be0794f964815c8
Кирилл Марков <kirill-markov-2020@bk.ru>
2026-04-06 20:30:22 +0300

    Сервисы из Program.cs вынесены в отдельный класс

--- ProjectCenter.API/Extensions/ServiceCollectionExtensions.cs
+++ ProjectCenter.API/Extensions/ServiceCollectionExtensions.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using ProjectCenter.Application.Interfaces;
+using ProjectCenter.Application.Mappings;
+using ProjectCenter.Application.Services;
 using ProjectCenter.Core.Exceptions;
+using ProjectCenter.Infrastructure.Persistence.Contexts;
+using ProjectCenter.Infrastructure.Persistence.Repositories;
+using ProjectCenter.Infrastructure.Services;
 using System.Text;
 
 namespace ProjectCenter.API.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+
         public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
         {
             var key = Encoding.UTF8.GetBytes(configuration["Jwt:Key"]);
@@ -20,14 +28,14 @@ namespace ProjectCenter.API.Extensions
                     {
                         ValidateIssuer = true,
                         ValidateAudience = true,
-                        ValidateLifetime = true,                
+                        ValidateLifetime = true,
                         ValidateIssuerSigningKey = true,
 
                         ValidIssuer = configuration["Jwt:Issuer"],
                         ValidAudience = configuration["Jwt:Audience"],
                         IssuerSigningKey = new SymmetricSecurityKey(key),
 
-                        ClockSkew = TimeSpan.Zero          
+                        ClockSkew = TimeSpan.Zero
                     };
                     options.Events = new JwtBearerEvents
                     {
@@ -46,6 +54,78 @@ namespace ProjectCenter.API.Extensions
 
             return services;
         }
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IDirectoryRepository, DirectoryRepository>();
+            services.AddScoped<ITeacherRepository, TeacherRepository>();
+            services.AddScoped<IGradeRepository, GradeRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IProjectService, ProjectService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IDirectoryService, DirectoryService>();
+            services.AddScoped<ITeacherService, TeacherService>();
+            services.AddScoped<IGradeService, GradeService>();
+            services.AddScoped<IJwtService, JwtService>();
+            services.AddScoped<IFileService, FileService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+                cfg.AddProfile<UserProfile>();
+            });
+
+            return services;
+        }
+
+  
+        public static IServiceCollection AddCustomCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                });
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddAllServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDatabase(configuration);
+            services.AddJwtAuthentication(configuration);
+            services.AddRepositories();
+            services.AddApplicationServices();
+            services.AddCustomAutoMapper();
+            services.AddCustomCors();
+
+            return services;
+        }
     }
 }
--- ProjectCenter.API/Program.cs
+++ ProjectCenter.API/Program.cs
@@ -1,78 +1,34 @@
-﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
-using ProjectCenter.API.Extensions;
+﻿using ProjectCenter.API.Extensions;
 using ProjectCenter.API.Middleware;
-using ProjectCenter.Application.Interfaces;
-using ProjectCenter.Application.Mappings;
-using ProjectCenter.Application.Services;
-using ProjectCenter.Core.Entities;
-using ProjectCenter.Core.Services;
-using ProjectCenter.Infrastructure.Persistence.Contexts;
-using ProjectCenter.Infrastructure.Persistence.Repositories;
-using ProjectCenter.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Регистрация всех сервисов одной строкой
+builder.Services.AddAllServices(builder.Configuration);
 
-builder.Services.AddJwtAuthentication(builder.Configuration);
-builder.Services.AddScoped<IDirectoryRepository, DirectoryRepository>();
-builder.Services.AddScoped<IDirectoryService, DirectoryService>();
-builder.Services.AddScoped<ITeacherRepository, TeacherRepository>();
-builder.Services.AddScoped<ITeacherService, TeacherService>();
-builder.Services.AddScoped<IGradeRepository, GradeRepository>();
-builder.Services.AddScoped<IGradeService, GradeService>();
-builder.Services.AddScoped<IJwtService, JwtService>();
-builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-builder.Services.AddScoped<IProjectService, ProjectService>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IFileService, FileService>();
-
-
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new CustomDateTimeConverter());
-    });
-
-builder.Services.AddAutoMapper(cfg =>
-{
-    cfg.AddProfile<MappingProfile>();
-    cfg.AddProfile<UserProfile>();
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins("http://localhost:5173")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
-});
-
+// Добавляем контроллеры
 builder.Services.AddControllers();
+
+// Добавляем Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerWithBearer();
 
 var app = builder.Build();
-app.UseMiddleware<ProjectCenter.API.Middleware.ExceptionHandlingMiddleware>();
+
+// Middleware
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors();
+
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseCors();
 
+// Настройка статических файлов
 app.UseStaticFiles(new StaticFileOptions
 {
     OnPrepareResponse = ctx =>
@@ -83,8 +39,7 @@ app.UseStaticFiles(new StaticFileOptions
     }
 });
 
-app.UseMiddleware<ProjectCenter.API.Middleware.UserContextMiddleware>();
-
+app.UseMiddleware<UserContextMiddleware>();
 app.MapControllers();
 
 app.Run();
